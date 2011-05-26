02121826712ce0269dad628f449f008a1d980fc6
rackom <rackom10@gmail.com>
2011-05-26 20:23:41 +0200

    oprava chybneho selectu pokus 10

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -4,6 +4,6 @@
 class Screening < ActiveRecord::Base
   def self.get_percent_filling_by_movie_id(movieID)
     #Screening.order("time_before_screening").find_all_by_movie_id(movieID)
-    find_by_sql(["select avg(seats_taken/seats_total) as obsadenost, time_before_screening from screenings where movie_id = ? group by floor(time_before_screening / 1)", movieID])
+    find_by_sql(["select floor(time_before_screening/1) as time_before_screening, avg(seats_taken/seats_total) as obsadenost from screenings where movie_id = ? group by time_before_screening", movieID])
   end
 end
