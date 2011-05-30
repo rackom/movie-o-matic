5fccc46c1aea9ce27687db83214e64d495f0ebb1
rackom <rackom10@gmail.com>
2011-05-30 09:39:38 +0200

    opraveny sql request na postgre

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -4,7 +4,7 @@
 class Screening < ActiveRecord::Base
   def self.get_percent_filling_by_movie_id(movieID)
     #Screening.order("time_before_screening").find_all_by_movie_id(movieID)
-    find_by_sql(["select cast(floor(time_before_screening/1) as decimal(7,1)) as tbs, avg(seats_taken/seats_total) as obsadenost from screenings where movie_id = ? group by tbs;", movieID])
+    find_by_sql(["select cast(floor(time_before_screening::real/1) as decimal(7,1)) as tbs, avg(seats_taken::real/seats_total) as obsadenost from screenings where movie_id = ? group by tbs;", movieID])
   end
 end
 
